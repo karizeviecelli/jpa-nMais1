e5a3e0e473b2c743c90f57d9476b7b4678e9d80c
karizeviecelli <karizeviecellisc@gmail.com>
2025-12-11 15:53:38 -0300

    git security checklist, not tested

--- aula 05/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/Role.java
+++ aula 05/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/Role.java
@@ -1,13 +1,15 @@
 package com.devsuperior.demo.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
+    @SuppressWarnings("serial")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -29,6 +31,7 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
